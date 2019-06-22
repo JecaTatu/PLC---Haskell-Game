b0db1f2866d22938b731ddacc79a39a595a314ec
JecaTatu <gas5@cin.ufpe.br>
2019-06-22 14:51:57 -0300

    base states to run the game

--- Snake.hs
+++ Snake.hs
@@ -85,3 +85,42 @@ game :: Level -> Score -> [Char] -> IO State
 game level score checker = clearScreen
     >> firstState score level
     >>= (iterateUntilM gameOver (step score checker))
+
+-- Estado inicial de cada jogo, ele recebe o level e o score do jogo e retorna um estado
+-- Este estado vai ter informações como posição dos players, do primeiro good, entre outras informações
+-- Que são necessárias para o desenvolver do jogo
+
+baseState :: Score -> Level -> IO State
+baseState score level = do
+                    (a,b) <- takeMVar score
+                    putMVar score (a,b)
+                    l <- takeMVar level
+                    putMVar level l
+                    stdGen <- getStdGen 
+                    return State {
+                board = 30, -- Tamanho da tela 
+                player1 = [(4, 24)], --Posição do primeiro player
+                player2 = [(49, 27)], -- Posição do segundo player
+                std = stdGen,
+                good = [randomPosition (concat (boardPositions 30)) stdGen], -- Cria um elemento bom em uma posição randomica em algum lugar da tela
+                evil = [], -- No estado inicial não possui elementos maus
+                surprise = [], -- No estado inicial não possui elementos surpresa
+                move1  = Just (0, 1, 0),
+                move2  = Just (1, -1, 0),
+                points1 = a,
+                points2 = b,
+                level = l,
+                count = 0
+            }
+
+-- Retorna um array de tuplas com todas as posições da tela;
+boardPositions :: Int -> [[(Int, Int)]]
+boardPositions size
+    = [[(x, y) | x <- [0 .. (size*2 - 1)]] | y <- reverse [0 .. (size - 1)]]
+
+-- Recebe um array e retorna uma posição aleatória do mesmo, é usado para
+-- Geração aleatória de elementos tanto pra elementos positivos quanto para negativos
+randomPosition :: [a] -> StdGen -> a
+randomPosition xs inputStdGen = element
+    where indexStdGenTuple = randomR (0, length xs - 1) inputStdGen
+          element          = xs !! fst (indexStdGenTuple)
