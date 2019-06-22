e386de80167458696e6c8e5e4583d6fa837095e3
JecaTatu <gas5@cin.ufpe.br>
2019-06-22 14:37:34 -0300

    adding the imports

---
+++ Snake.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import System.IO
+import Data.List
+import System.Timeout
+import System.Random
+import Control.Applicative
+import Control.Monad.Loops
+import System.Console.ANSI
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Concurrent.Async
