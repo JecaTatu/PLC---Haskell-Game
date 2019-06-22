3c68d1e5c25ef981f72591be71715d17a87870b6
JecaTatu <gas5@cin.ufpe.br>
2019-06-22 14:38:59 -0300

    creating the base of variables of th eproject

--- Snake.hs
+++ Snake.hs
@@ -10,3 +10,34 @@ import System.Console.ANSI
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Concurrent.Async
+
+-- Tipo representando uma tupla de inteiros
+type Vector = (Int, Int)
+-- MVAR que controla os valores do placar
+type Score = MVar (Int, Int)
+-- MVar representando o level que é incrementado a cada começo de partida
+type Level = MVar Int
+-- Tipo representando as coordenadas de movimento, em forma de uma tripla de inteiros
+type MoveVector = (Int, Int, Int)
+
+data State = State {
+    -- Tamanho da tela
+    board :: Int,
+    -- Um array com as posições de cada player
+    player1 :: [Vector],
+    player2 :: [Vector],
+    -- Arrays com as posições de cada item extra na tela
+    evil :: [Vector],
+    good :: [Vector],
+    surprise :: [Vector],
+    -- Uma chave para gerar número randomicos
+    std :: StdGen,
+    -- A direção e o sentido dos players
+    move1 :: Maybe MoveVector,
+    move2 :: Maybe MoveVector,
+    -- As pontuações de cada jogador
+    points1 :: Int,
+    points2 :: Int,
+    level :: Int,
+    count :: Int
+} deriving Show
